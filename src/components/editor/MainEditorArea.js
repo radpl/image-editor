7b7903660fd18673452c0a7d7e2f84ecf88c72d4
Radoslaw Plonka <radoslaw.a.plonka@gmail.com>
2019-11-16 01:44:12 +0100

    Started drag drop

--- src/components/editor/MainEditorArea.js
+++ src/components/editor/MainEditorArea.js
@@ -1,36 +1,89 @@
-import React from 'react';
-import { useDrop } from 'react-dnd';
+import React, { Component } from 'react';
+//import { useDrop } from 'react-dnd';
+import { DropTarget } from "react-dnd";
 import ItemTypes from '../dnd/ItemTypes';
 import SimpleButton from '../common/SimpleButton';
-export default function MainEditorArea(props) {
+import LogoElement from '../logo/LogoElement';
+import background from '../../assets/bgAssets';
+import update from 'immutability-helper'
 
-    const [{ canDrop, isOver }, drop] = useDrop({
-        accept: ItemTypes.LOGO,
-        drop: () => ({ name: 'MainEditorArea' }),
-        collect: monitor => ({
-            isOver: monitor.isOver(),
-            canDrop: monitor.canDrop(),
-        }),
-    });
+class MainEditorArea extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            logos: {}
+        }
+    }
 
-    const isActive = canDrop && isOver;
+    moveBox(id, left, top) {
+        // this.setState(
+        //     update(this.state, {
+        //         logos: {
+        //             [id]: {
+        //                 $merge: { left, top },
+        //             },
+        //         },
+        //     }),
+        // )
+    }
 
-    const style = {
-        width: "400px",
-        height: "400px",
-        backgroundColor: "white",
-        textAlign: "center",
-        marginTop: "2em",
-        backgroundImage: `url(${props.selectedBackground})`
-    };
 
-    return (
-        <>
-            <div ref={drop} style={style}>
-                {isActive ? 'Release to drop' : 'Drag a logo here'}
+    render() {
+        const { isOver, canDrop, connectDropTarget } = this.props;
+        const { logos } = this.state;
+        console.log(isOver, canDrop);
+        const style = {
+            width: "400px",
+            height: "400px",
+            backgroundColor: "white",
+            textAlign: "center",
+            marginTop: "2em",
+            backgroundImage: `url(${this.props.selectedBackground})`
+        };
+
+        const imageStyle = {
+            position: "absolute",
+            width: "100px",
+            height: "100px",
+        }
+        return connectDropTarget(
+            <div>
+                <div style={style}>
+                    {Object.keys(logos).map(key => {
+                        const { left, top } = logos[key]
+                        return (
+                            <LogoElement
+                                key={key}
+                                id={key}
+                                left={left}
+                                top={top}
+                                hideSourceOnDrag="true"
+                                element={imageStyle}
+                            />
+                        )
+                    })}
+                </div>
+                <SimpleButton>Download as image</SimpleButton>
             </div>
-            <SimpleButton>Download as image</SimpleButton>
-        </>
-    )
+        );
+    }
 }
-
+export default DropTarget(ItemTypes.LOGO,
+    {
+        drop(props, monitor, component) {
+            if (!component) {
+                return;
+            }
+            const item = monitor.getItem();
+            const delta = monitor.getDifferenceFromInitialOffset();
+            const left = Math.round(item.left + delta.x);
+            const top = Math.round(item.top + delta.y);
+            component.moveBox(item.id, left, top);
+        }
+    },
+    (connect, monitor) => ({
+        connectDropTarget: connect.dropTarget(),
+        isOver: monitor.isOver(),
+        canDrop: monitor.canDrop(),
+    })
+)(MainEditorArea);
